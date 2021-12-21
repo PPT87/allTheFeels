016d8a417c629dafc59237f6e83bc608831c5019
PeterT <tranpp87@gmail.com>
2021-12-20 22:26:21 -0500

    reroute login and signup to /posts

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -20,7 +20,7 @@ const LoginForm = props => {
     try {
       await authService.login(formData)
       props.handleSignupOrLogin()
-      navigate('/main')
+      navigate('/posts')
     } catch (err) {
       props.updateMessage(err.message)
     }
