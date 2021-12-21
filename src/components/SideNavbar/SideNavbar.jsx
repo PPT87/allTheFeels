a7abe5580243e5191382764b76751447b8be2a51
PeterT <tranpp87@gmail.com>
2021-12-20 22:21:03 -0500

    add icons and update log out navbar

--- src/components/SideNavbar/SideNavbar.jsx
+++ src/components/SideNavbar/SideNavbar.jsx
@@ -1,5 +1,10 @@
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo/logo-desktop.png'
+import { GoHome } from 'react-icons/go'
+import { BsPerson, BsPersonPlus } from 'react-icons/bs'
+import { AiOutlinePlusCircle } from 'react-icons/ai'
+import { BiLogOut } from 'react-icons/bi'
+import { VscSignIn } from 'react-icons/vsc'
 
 
 const SideNavbar = ({ user, handleLogout, showNav }) =>{
@@ -12,18 +17,18 @@ return(
           <img src={logo} alt="logo" className='logo'/>
           <ul>
             <li>Welcome, {user.name}</li>
-            <li><Link to={`/profile/${user.profile}`}>Profile</Link></li>
-            <li><Link to="/posts">Home</Link></li>
-            <li><Link to="/new">Add Post</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+            <li><Link to="/posts"><GoHome /> Home</Link></li>
+            <li><Link to={`/profile/${user.profile}`}><BsPerson /> Profile</Link></li>
+            <li><Link to="/new"><AiOutlinePlusCircle /> Add Post</Link></li>
+            <li><Link to="" onClick={handleLogout}><BiLogOut /> LOG OUT</Link></li>
           </ul>
         </nav>
       :
-        <nav>
+        <nav className={showNav ? 'sidenav active' : 'sidenav'}>  
           <ul>
-            <li><Link to="/posts">Home</Link></li>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
+            <li><Link to="/posts"><GoHome /> Home</Link></li>
+            <li><Link to="/login"><VscSignIn /> Log In</Link></li>
+            <li><Link to="/signup"><BsPersonPlus /> Sign Up</Link></li>
           </ul>
         </nav>
       }
