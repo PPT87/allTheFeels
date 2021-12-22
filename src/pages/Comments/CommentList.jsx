144c8cab5a500fc4af44007f1d7c105c0b74d2a0
Payal Desai <payal@idesai.com>
2021-12-22 12:46:22 -0500

    css updates

--- src/pages/Comments/CommentList.jsx
+++ src/pages/Comments/CommentList.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-
+import "./CommentSection.css"
 
 const CommentList = (props) => {
   const authorId = props.comment.commenter?._id ? props.comment.commenter._id : props.comment.commenter
--- src/pages/Comments/CreateComment.jsx
+++ src/pages/Comments/CreateComment.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-
+import "./CommentSection.css"
 
 const CreateComment = (props) => {
 	const [text, setText] = useState('')
